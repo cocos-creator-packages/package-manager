d4cb9811cf79b2ec515caa251923cdbdf9ca9937
Johnny Wu <KarasAya@gmail.com>
2015-05-23 03:03:10 +0800

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # package-manager
+
 Package Manager for Editor Framework
---
+++ main.js
@@ -0,0 +1,11 @@
+module.exports = {
+    load: function () {
+    },
+
+    unload: function () {
+    },
+
+    'package-manager:open': function () {
+        Editor.Panel.open('package-manager.panel');
+    },
+};
