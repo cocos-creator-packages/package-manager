8fb63b82159ae42a6901d8da1db85d93718ea0c0
PP <chinese1236@qq.com>
2019-05-20 18:36:57 +0800

    refactor package-manager with Vue (#6)

--- panel/package-manager.js
+++ panel/package-manager.js
@@ -1,112 +1,128 @@
-(function () {
-  'use strict';
+const fs = require('fs');
 
-  var _ = require('lodash');
+var _ = require('lodash');
 
-  function _createPackageInfo ( result ) {
-    return {
-      enabled: result.enabled,
-      hasTests: result.info.tests && result.info.tests.length > 0,
-      info: result.info,
-    };
-  }
+function _createPackageInfo(result) {
+  return {
+    enabled: result.enabled,
+    hasTests: result.info.tests && result.info.tests.length > 0,
+    info: result.info,
+  };
+}
 
-  Editor.polymerPanel( 'package-manager', {
-    properties: {
-      filterText: {
-        type: String,
-        value: ''
-      }
+function createVue(elem) {
+  return new window.Vue({
+    el: elem,
+
+    data: {
+      filterText: '',
+      packages: [],
+      filterPackages: [],
     },
 
-    ready: function () {
-      Editor.Package.queryInfos(function ( err, results ) {
-        var packages = results.map( function (item) {
+    compiled() {
+      let self = this;
+      Editor.Package.queryInfos(function (err, results) {
+        var packages = results.map(function (item) {
           return _createPackageInfo(item);
         });
-        this.set( 'packages', packages );
-      }.bind(this));
-    },
-
-    focusOnSearch: function ( event ) {
-      if ( event ) {
-        event.stopPropagation();
-      }
-
-      Editor.UI.focus(this.$.search);
-    },
-
-    messages: {
-      'editor:package-loaded': function ( event, name ) {
-        Editor.Package.queryInfo(name, function ( err, result ) {
-          this.push( 'packages', _createPackageInfo(result));
-        }.bind(this));
-      },
-
-      'editor:package-unloaded': function ( event, name ) {
-        var idx = _.findIndex( this.packages, function ( item ) {
-          return item.info.name === name;
-        });
-        this.splice( 'packages', idx, 1 );
-      },
-    },
-
-    _onReload: function (event) {
-      event.stopPropagation();
-
-      var model = this.$.list.modelForElement(event.target);
-      var oldname = model.item.info.name;
-      Editor.Package.reload(oldname);
-    },
-
-    _onTest: function (event) {
-      event.stopPropagation();
-
-      var item = this.$.list.itemForElement(event.target);
-      Editor.Panel.open( 'tester', {
-        name: item.info.name,
+        self.packages = packages;
+        self._applyFilter();
       });
     },
 
-    _enabledText: function (enabled) {
-      if (enabled) {
-        return 'Disable';
-      }
-      else {
-        return 'Enable';
-      }
-    },
-
-    _sortPackages: function ( a, b ) {
-      return a.info.name.localeCompare( b.info.name );
-    },
-
-    _applyFilter: function (packages,filterText) {
-      if (!filterText) {
-        this.$.view.hidden = false;
-        this.$.none.hidden = true;
-
-        return packages;
-      }
-
-      var tmpPackages = [];
-      var filter = filterText.toLowerCase();
-      for (var i = 0; i < packages.length; ++i) {
-        if (packages[i].info.name.toLowerCase().match(filter)) {
-          tmpPackages.push(packages[i]);
+    methods: {
+      focusOnSearch (event) {
+        if (event) {
+          event.stopPropagation();
         }
-      }
-      if (tmpPackages.length > 0) {
-        this.$.view.hidden = false;
-        this.$.none.hidden = true;
-      }
-      else {
-        this.$.view.hidden = true;
-        this.$.none.hidden = false;
-      }
 
-      return tmpPackages;
+        Editor.UI.focus(this.$els.search);
+      },
+
+      _onReload (item) {
+        let pkgName = item.info.name;
+        Editor.Package.reload(pkgName);
+      },
+
+      _onTest (event) {
+        event.stopPropagation();
+
+        // var item = this.$els.list.itemForElement(event.target);
+        // Editor.Panel.open('tester', {
+        //   name: item.info.name,
+        // });
+
+        // TODO
+      },
+
+      _enabledText (enabled) {
+        if (enabled) {
+          return 'Disable';
+        }
+        else {
+          return 'Enable';
+        }
+      },
+
+      _sortPackages (a, b) {
+        return a.info.name.localeCompare(b.info.name);
+      },
+
+      _applyFilter () {
+        let packages = this.packages;
+        let filterText = this.filterText;
+
+        if (!filterText) {
+          // 更新过滤后的包列表
+          this.filterPackages = packages;
+          return;
+        }
+
+        var tmpPackages = [];
+        var filter = filterText.toLowerCase();
+        for (var i = 0; i < packages.length; ++i) {
+          if (packages[i].info.name.toLowerCase().match(filter)) {
+            tmpPackages.push(packages[i]);
+          }
+        }
+
+        // 更新过滤后的包列表
+        this.filterPackages = tmpPackages;
+        return;
+      },
+
+      _onSearchChange (event) {
+        let value = event.target.value;
+        this.filterText = value;
+        this._applyFilter();
+      },
     },
   });
+}
 
-})();
+Editor.Panel.extend({
+  template: fs.readFileSync(Editor.url('packages://package-manager/panel/package-manager.html'), 'utf8'),
+
+  style: fs.readFileSync(Editor.url('packages://package-manager/panel/package-manager.css'), 'utf8'),
+
+  ready() {
+    this._vm = createVue(this.shadowRoot);
+  },
+
+  messages: {
+    'editor:package-loaded' ( event, name ) {
+      let self = this;
+      Editor.Package.queryInfo(name, function ( err, result ) {
+        self._vm.packages.push(_createPackageInfo(result));
+      });
+    },
+
+    'editor:package-unloaded' ( event, name ) {
+      var idx = _.findIndex( this.packages, function ( item ) {
+        return item.info.name === name;
+      });
+      this._vm.packages.splice(idx, 1);
+    },
+  },
+});
